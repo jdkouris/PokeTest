083e4713205196e09f420ad9de333dc9b96d7179
John Kouris <jdkouris@gmail.com>
2021-06-20 08:40:41 -0700

    Initial Commit

---
+++ PokeTest/PokeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeTestApp.swift
+//  PokeTest
+//
+//  Created by John Kouris on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct PokeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
