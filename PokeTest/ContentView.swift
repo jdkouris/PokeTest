ffc05460206ff46af182270dccd71d5df730daba
John Kouris <jdkouris@gmail.com>
2021-06-20 10:42:47 -0700

    Finished styling

--- PokeTest/ContentView.swift
+++ PokeTest/ContentView.swift
@@ -13,24 +13,59 @@ struct ContentView: View {
     @State private var pokemon = [Pokemon]()
     
     var body: some View {
-        List(pokemon) { poke in
-            HStack {
-                Text(poke.name.capitalized)
-                
-                Spacer()
-                
-                VStack(alignment: .trailing) {
-                    KFImage(URL(string: poke.imageURL))
-                        .interpolation(.none)
-                        .resizable()
-                        .frame(width: 100, height: 100)
+        NavigationView {
+            List(pokemon) { poke in
+                HStack {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(poke.name.capitalized)
+                            .font(.title)
+                        HStack {
+                            Text(poke.type.capitalized)
+                                .italic()
+                            Circle()
+                                .foregroundColor(poke.typeColor)
+                                .frame(width: 10, height: 10)
+                        }
+                        Text(poke.description)
+                            .font(.caption)
+                            .lineLimit(2)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+//                        KFImage(URL(string: poke.imageURL))
+//                            .interpolation(.none)
+//                            .resizable()
+//                            .frame(width: 100, height: 100)
+                        
+                        AsyncImage(url: URL(string: poke.imageURL)) { phase in
+                            switch phase {
+                            case .empty:
+                                ProgressView()
+                            case .success(let image):
+                                image.resizable()
+                                    .interpolation(.none)
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                            case .failure:
+                                Image(systemName: "photo")
+                            @unknown default:
+                                EmptyView()
+                            }
+                        }
+                    }
                 }
             }
-        }
-        .onAppear {
-            async {
-                pokemon = try! await pokemonModel.getPokemon()
+            .navigationTitle("Pokemon")
+            .onAppear {
+                async {
+                    pokemon = try! await pokemonModel.getPokemon()
+                }
             }
+//            .refreshable {
+//                pokemon = try! await pokemonModel.getPokemon()
+//            }
         }
     }
 }
